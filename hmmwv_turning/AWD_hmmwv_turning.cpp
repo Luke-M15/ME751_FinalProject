252ce0d3fb13c1db157e9da8f65a8d1d5292492f
Luke-M15 <mottley15@gmail.com>
2019-12-10 21:57:21 -0600

    3 different vehicle configurations for comparison

--- hmmwv_turning/AWD_hmmwv_turning.cpp
+++ hmmwv_turning/AWD_hmmwv_turning.cpp
@@ -18,6 +18,7 @@
 
 #include "chrono_models/vehicle/hmmwv/HMMWV.h"
 
+#include "chrono_postprocess/ChGnuPlot.h"
 #include "chrono_thirdparty/filesystem/path.h"
 
 using namespace chrono;
@@ -80,6 +81,9 @@ float mu_t = 0.8f;
 // Simulation parameters
 // -----------------------------------------------------------------------------
 
+// Simulation Length
+double t_end = 8.5;
+
 // Simulation step size
 double step_size = 3e-3;
 
@@ -90,11 +94,11 @@ double render_step_size = 1.0 / 100;
 ChVector<> trackPoint(0.0, 0.0, 1.75);
 
 // Output directories
-const std::string out_dir = "HMMWV_defSoil";
+const std::string out_dir = "HMMWV_AWD";
 const std::string img_dir = out_dir + "/IMG";
 
 // Visualization output
-bool img_output = true;
+bool img_output = false;
 
 // =============================================================================
 
@@ -119,7 +123,7 @@ public:
 		else
 			m_throttle = 3.5 * eff_time;
 
-		if (eff_time < 2)
+		if (eff_time < 1)
 			m_steering = 0;
 		else
 			m_steering = 0.6 * std::sin(CH_C_2PI * (eff_time - 2) / 6);
@@ -132,10 +136,11 @@ private:
 // =============================================================================
 
 void CreateLuggedGeometry(std::shared_ptr<ChBody> wheelBody) {
-	std::string lugged_file("hmmwv/lugged_wheel_section.obj");
+
+	std::string lugged_file("vehicle/hmmwv/lugged_wheel_section.obj");
 	geometry::ChTriangleMeshConnected lugged_mesh;
 	ChConvexDecompositionHACDv2 lugged_convex;
-	utils::LoadConvexMesh(vehicle::GetDataFile(lugged_file), lugged_mesh, lugged_convex);
+	utils::LoadConvexMesh(GetChronoDataFile(lugged_file), lugged_mesh, lugged_convex);
 	int num_hulls = lugged_convex.GetHullCount();
 
 	auto coll_model = wheelBody->GetCollisionModel();
@@ -158,7 +163,7 @@ void CreateLuggedGeometry(std::shared_ptr<ChBody> wheelBody) {
 
 	// Visualization
 	auto trimesh = std::make_shared<geometry::ChTriangleMeshConnected>();
-	trimesh->LoadWavefrontMesh(vehicle::GetDataFile("hmmwv/lugged_wheel.obj"), false, false);
+	trimesh->LoadWavefrontMesh(GetChronoDataFile("vehicle/hmmwv/lugged_wheel.obj"), false, false);
 
 	auto trimesh_shape = std::make_shared<ChTriangleMeshShape>();
 	trimesh_shape->SetMesh(trimesh);
@@ -173,6 +178,8 @@ void CreateLuggedGeometry(std::shared_ptr<ChBody> wheelBody) {
 
 int main(int argc, char* argv[]) {
 	GetLog() << "Copyright (c) 2017 projectchrono.org\nChrono version: " << CHRONO_VERSION << "\n\n";
+	// Set path to Chrono data directory
+	SetChronoDataPath(CHRONO_DATA_DIR);
 
 	// --------------------
 	// Create HMMWV vehicle
@@ -209,7 +216,7 @@ int main(int argc, char* argv[]) {
 	// --------------------
 	// Create driver system
 	// --------------------
-	MyDriver driver(my_hmmwv.GetVehicle(), 0.5);
+	MyDriver driver(my_hmmwv.GetVehicle(), 0.25);
 	driver.Initialize();
 
 	// ------------------
@@ -217,58 +224,38 @@ int main(int argc, char* argv[]) {
 	// ------------------
 	ChTerrain* terrain;
 
-	switch (terrain_type) {
-	case DEFORMABLE_SOIL: {
-		SCMDeformableTerrain* terrainD = new SCMDeformableTerrain(system);
-		terrainD->SetPlane(ChCoordsys<>(VNULL, Q_from_AngX(CH_C_PI_2)));
-		terrainD->SetSoilParametersSCM(2e6,   // Bekker Kphi
-			0,     // Bekker Kc
-			1.1,   // Bekker n exponent
-			0,     // Mohr cohesive limit (Pa)
-			30,    // Mohr friction limit (degrees)
-			0.01,  // Janosi shear coefficient (m)
-			2e8,   // Elastic stiffness (Pa/m), before plastic yield
-			3e4    // Damping (Pa s/m), proportional to negative vertical speed (optional)
-		);
-		/*
-		terrainD->SetBulldozingFlow(true);    // inflate soil at the border of the rut
-		terrainD->SetBulldozingParameters(55, // angle of friction for erosion of displaced material at the border of the rut
-										0.8, // displaced material vs downward pressed material.
-										5,   // number of erosion refinements per timestep
-										10); // number of concentric vertex selections subject to erosion
-		*/
-		// Turn on the automatic level of detail refinement, so a coarse terrain mesh
-		// is automatically improved by adding more points under the wheel contact patch:
-		terrainD->SetAutomaticRefinement(true);
-		terrainD->SetAutomaticRefinementResolution(0.04);
+	
+	SCMDeformableTerrain* terrainD = new SCMDeformableTerrain(system);
+	terrainD->SetPlane(ChCoordsys<>(VNULL, Q_from_AngX(CH_C_PI_2)));
+	terrainD->SetSoilParametersSCM(2e6,   // Bekker Kphi
+		0,     // Bekker Kc
+		1.1,   // Bekker n exponent
+		0,     // Mohr cohesive limit (Pa)
+		30,    // Mohr friction limit (degrees)
+		0.01,  // Janosi shear coefficient (m)
+		2e8,   // Elastic stiffness (Pa/m), before plastic yield
+		3e4    // Damping (Pa s/m), proportional to negative vertical speed (optional)
+	);
+	/*
+	terrainD->SetBulldozingFlow(true);    // inflate soil at the border of the rut
+	terrainD->SetBulldozingParameters(55, // angle of friction for erosion of displaced material at the border of the rut
+									0.8, // displaced material vs downward pressed material.
+									5,   // number of erosion refinements per timestep
+									10); // number of concentric vertex selections subject to erosion
+	*/
+	// Turn on the automatic level of detail refinement, so a coarse terrain mesh
+	// is automatically improved by adding more points under the wheel contact patch:
+	terrainD->SetAutomaticRefinement(true);
+	terrainD->SetAutomaticRefinementResolution(0.04);
 
-		////terrainD->SetTexture(vehicle::GetDataFile("terrain/textures/grass.jpg"), 80, 16);
-		////terrainD->SetPlotType(vehicle::SCMDeformableTerrain::PLOT_PRESSURE_YELD, 0, 30000.2);
-		terrainD->SetPlotType(vehicle::SCMDeformableTerrain::PLOT_SINKAGE, 0, 0.1);
+	////terrainD->SetTexture(vehicle::GetDataFile("terrain/textures/grass.jpg"), 80, 16);
+	////terrainD->SetPlotType(vehicle::SCMDeformableTerrain::PLOT_PRESSURE_YELD, 0, 30000.2);
+	terrainD->SetPlotType(vehicle::SCMDeformableTerrain::PLOT_SINKAGE, 0, 0.1);
 
-		terrainD->Initialize(terrainHeight, terrainLength, terrainWidth, divLength, divWidth);
+	terrainD->Initialize(terrainHeight, terrainLength, terrainWidth, divLength, divWidth);
 
-		terrain = terrainD;
+	terrain = terrainD;
 
-		break;
-	}
-
-	case RIGID_SOIL: {
-		RigidTerrain* terrainR = new RigidTerrain(system);
-		auto patch = terrainR->AddPatch(ChCoordsys<>(ChVector<>(0, 0, terrainHeight - 5), QUNIT),
-			ChVector<>(terrainLength, terrainWidth, 10));
-		patch->SetContactFrictionCoefficient(0.9f);
-		patch->SetContactRestitutionCoefficient(0.01f);
-		patch->SetContactMaterialProperties(2e7f, 0.3f);
-		patch->SetColor(ChColor(0.8f, 0.8f, 0.5f));
-		patch->SetTexture(vehicle::GetDataFile("terrain/textures/tile4.jpg"), 200, 200);
-		terrainR->Initialize();
-
-		terrain = terrainR;
-
-		break;
-	}
-	}
 
 	// Complete system construction
 	system->SetupInitial();
@@ -297,7 +284,8 @@ int main(int argc, char* argv[]) {
 			return 1;
 		}
 	}
-
+	
+	utils::CSV_writer outFile(" ");
 	// ---------------
 	// Simulation loop
 	// ---------------
@@ -322,11 +310,20 @@ int main(int argc, char* argv[]) {
 	int render_frame = 0;
 	ChRealtimeStepTimer realtime_timer;
 
+	
 	while (app.GetDevice()->run()) {
 		double time = system->GetChTime();
 
-		////auto frc = static_cast<ChRigidTire*>(my_hmmwv.GetTire(0))->ReportTireForce(terrain);
-		////std::cout << frc.force.x() << " " << frc.force.y() << " " << frc.force.z() << std::endl;
+		if (time >= t_end)
+			break;
+		auto frcL = static_cast<ChRigidTire*>(my_hmmwv.GetTire(0))->ReportTireForce(terrain);
+		double longSL = my_hmmwv.GetTire(0)->GetLongitudinalSlip();
+		double slipL = my_hmmwv.GetTire(0)->GetSlipAngle();
+		auto frcR = static_cast<ChRigidTire*>(my_hmmwv.GetTire(1))->ReportTireForce(terrain);
+		double longSR = my_hmmwv.GetTire(1)->GetLongitudinalSlip();
+		double slipR = my_hmmwv.GetTire(1)->GetSlipAngle();
+		outFile << time << frcL.force.x() << frcL.force.y() << frcL.force.z() << longSL << slipL;
+		outFile << frcR.force.x() << frcR.force.y() << frcR.force.z() << longSR << slipR << std::endl;
 
 		// Render scene
 		app.BeginScene(true, true, irr::video::SColor(255, 140, 161, 192));
@@ -360,6 +357,63 @@ int main(int argc, char* argv[]) {
 
 		app.EndScene();
 	}
+	outFile.write_to_file(out_dir + "/output.dat");
+	// creating plots
+	std::string plot1 = out_dir + "/left_tire_y_force.gpl";
+	postprocess::ChGnuPlot mplot1(plot1.c_str());
+	mplot1.SetGrid();
+	mplot1.SetLabelX("Time (sec)");
+	mplot1.SetLabelY("Force (N)");
+	mplot1.Plot("output.dat", 1, 3, "Left Tire Y Force");
+
+	std::string plot2 = out_dir + "/left_tire_z_force.gpl";
+	postprocess::ChGnuPlot mplot2(plot2.c_str());
+	mplot2.SetGrid();
+	mplot2.SetLabelX("Time (sec)");
+	mplot2.SetLabelY("Force (N)");
+	mplot2.Plot("output.dat", 1, 4, "Left Tire Z Force");
+
+	std::string plot3 = out_dir + "/right_tire_y_force.gpl";
+	postprocess::ChGnuPlot mplot3(plot3.c_str());
+	mplot3.SetGrid();
+	mplot3.SetLabelX("Time (sec)");
+	mplot3.SetLabelY("Force (N)");
+	mplot3.Plot("output.dat", 1, 8, "Right Tire Y Force");
+
+	std::string plot4 = out_dir + "/right_tire_z_force.gpl";
+	postprocess::ChGnuPlot mplot4(plot4.c_str());
+	mplot4.SetGrid();
+	mplot4.SetLabelX("Time (sec)");
+	mplot4.SetLabelY("Force (N)");
+	mplot4.Plot("output.dat", 1, 9, "Right Tire Z Force");
+
+	std::string plot5 = out_dir + "/left_tire_long_slip.gpl";
+	postprocess::ChGnuPlot mplot5(plot5.c_str());
+	mplot5.SetGrid();
+	mplot5.SetLabelX("Time (sec)");
+	mplot5.SetLabelY("Longitudinal Slip");
+	mplot5.Plot("output.dat", 1, 5, "Left Tire Longitudinal Slip");
+
+	std::string plot6 = out_dir + "/right_tire_long_slip.gpl";
+	postprocess::ChGnuPlot mplot6(plot6.c_str());
+	mplot6.SetGrid();
+	mplot6.SetLabelX("Time (sec)");
+	mplot6.SetLabelY("Longitudinal Slip");
+	mplot6.Plot("output.dat", 1, 10, "Right Tire Longitudinal Slip");
+
+	std::string plot7 = out_dir + "/left_tire_slip_angle.gpl";
+	postprocess::ChGnuPlot mplot7(plot7.c_str());
+	mplot7.SetGrid();
+	mplot7.SetLabelX("Time (sec)");
+	mplot7.SetLabelY("Slip Angle (deg)");
+	mplot7.Plot("output.dat", 1, 6, "Left Tire Slip Angle");
+
+	std::string plot8 = out_dir + "/right_tire_slip_angle.gpl";
+	postprocess::ChGnuPlot mplot8(plot8.c_str());
+	mplot8.SetGrid();
+	mplot8.SetLabelX("Time (sec)");
+	mplot8.SetLabelY("Slip Angle (deg)");
+	mplot8.Plot("output.dat", 1, 11, "Right Tire Slip Angle");
 
 	// Cleanup
 	delete terrain;
